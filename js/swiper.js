90d6d1eff44574208841b899359b4b9a7f2767f8
DevGirlAos40 <devgirlaos40@gmail.com>
2024-11-01 09:11:28 -0300

    add

--- js/swiper.js
+++ js/swiper.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const swiper = new Swiper('.swiper', {
      spaceBetween: 1,
     loop: true,
@@ -17,24 +16,4 @@ const swiper = new Swiper('.swiper', {
       scrollbar: {
         el: '.swiper-scrollbar',
       }
-=======
-const swiper = new Swiper('.swiper', {
-     spaceBetween: 1,
-    loop: true,
-    slidesPerView: 3,
-    centeredSlides: true,
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-    }
-    ,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      }
-      ,
-      scrollbar: {
-        el: '.swiper-scrollbar',
-      }
->>>>>>> b7b12c81790f57bd3d3c80523a7c207f6b417904
 });
